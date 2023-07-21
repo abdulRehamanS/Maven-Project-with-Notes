94a6d25249a5d6f54699283f2cc6a985df3b8a71
abdulRehamanS <abdulrehaman627@gmail.com>
2023-07-21 09:44:46 +0530

    adding maven project and notes

---
+++ Multi Modules/Multi-Modules/src/main/java/In/Abdul/Multi/Modules/MultiModulesApplication.java
@@ -0,0 +1,13 @@
+package In.Abdul.Multi.Modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModulesApplication.class, args);
+	}
+
+}
---
+++ Multi Modules/Multi-Modules/src/test/java/In/Abdul/Multi/Modules/MultiModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package In.Abdul.Multi.Modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
